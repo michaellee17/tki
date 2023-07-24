99bf72059ba739537a0692afdd97d706c5fa1567
michaellee17 <missing821017@yahoo.com.tw>
2023-07-24 16:59:38 +0800

    add:收藏搜尋串接分頁

--- src/components/PaginationA.vue
+++ src/components/PaginationA.vue
@@ -1,18 +1,42 @@
 <template>
-    <ul class="pagination d-flex">
-      <li><a href="#">&lt;</a></li>
-      <li><a href="#" class="active">1</a></li>
-      <li><a href="#">2</a></li>
-      <li><a href="#">3</a></li>
-      <li><a href="#">&gt;</a></li>
-    </ul>
+  <ul class="pagination d-flex">
+    <li>
+      <a href="#" :disabled="currentPage === 1" class="page_link" @click.prevent="changePage(currentPage - 1)" >&lt;</a>
+    </li>
+    <li v-for="page in totalPages" :key="page" :class="{ active: currentPage === page }">
+      <a href="#" @click.prevent="changePage(page)">{{ page }}</a>
+    </li>
+    <li>
+      <a href="#" :disabled="currentPage === totalPages" class="page_link" @click.prevent="changePage(currentPage + 1)">&gt;</a>
+    </li>
+  </ul>
 </template>
-
+<script>
+export default {
+  props: {
+    currentPage: {
+      type: Number,
+      required: true,
+    },
+    totalPages: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    changePage(page) {
+      if (page >= 1 && page <= this.totalPages) {
+        this.$emit('page-changed', page);
+      }
+    },
+  },
+};
+</script>
 <style scoped>
-.pagination li{
+.pagination li {
   padding: 5px;
 }
-.pagination li a.active{
+.pagination li.active a {
   color: var(--primary-color);
 }
 </style>
--- src/views/main/Member/MyCollection.vue
+++ src/views/main/Member/MyCollection.vue
@@ -1,41 +1,74 @@
 <template>
   <h3 class="text-primary mb-4">我的收藏</h3>
   <section class="d-flex flex-wrap gap-4 mb-3">
-    <div class="collection bg-cover text-white position-relative">
+    <div v-for="item in paginatedLists" :key="item.event_id" class="collection bg-cover text-white position-relative">
+      <img :src="item.main_imageH_url" alt="Event Image">
       <div class="position-absolute bottom-0">
-        <h4 class="fw-bold">楊丞琳</h4>
-        <p>世界巡回演唱會</p>
-      </div>
-    </div>
-    <div class="collection bg-cover text-white position-relative">
-      <div class="position-absolute bottom-0">
-        <h4 class="fw-bold">楊丞琳</h4>
-        <p>世界巡回演唱會</p>
-      </div>
-    </div>
-    <div class="collection bg-cover text-white position-relative">
-      <div class="position-absolute bottom-0">
-        <h4 class="fw-bold">楊丞琳</h4>
-        <p>世界巡回演唱會</p>
-      </div>
-    </div>
-    <div class="collection bg-cover text-white position-relative">
-      <div class="position-absolute bottom-0">
-        <h4 class="fw-bold">楊丞琳</h4>
-        <p>世界巡回演唱會</p>
+        <h4 class="fw-bold">{{ item.performer}}</h4>
+        <p>{{ item.event_name }}</p>
       </div>
     </div>
   </section>
   <div class="d-flex justify-content-end">
-    <PaginationA />
+    <PaginationA :total-pages="totalPages" :current-page="currentPage" @page-changed="changePage" />
   </div>
 </template>
-
 <script>
+import { mapGetters } from "vuex";
 import PaginationA from "../../../components/PaginationA.vue";
 
 export default {
-  components: { PaginationA }
+  components: { PaginationA },
+  data(){
+    return{
+      lists:[], //列表
+      currentPage: 1, // 當前分頁
+      itemsPerPage: 2, // 每頁顯示的項目數量
+    }
+  },
+  computed: {
+    ...mapGetters('user', ['getMemberData', 'getLoginData', 'getMemberBinding']),
+    // 計算總頁數
+    totalPages() {
+        return Math.ceil(this.lists.length / this.itemsPerPage);
+    },
+    paginatedLists() {
+      const startIdx = (this.currentPage - 1) * this.itemsPerPage;
+      const endIdx = startIdx + this.itemsPerPage;
+      return this.lists.slice(startIdx, endIdx);
+    },
+  },
+  mounted(){
+    this.getColletions()
+  },
+  methods: {
+    changePage(page) {
+      // 分頁變更事件處理器
+      this.currentPage = page;
+    },
+    getColletions(){
+      const apiUrl = `${process.env.VUE_APP_PATH}/user/get-collect-list`;
+      const accessToken = this.getLoginData.access_token
+      const requestData = {
+        limit:4, //總回傳筆數，預設為4
+        page:1, //總回傳頁數，預設為1
+      };
+      this.axios.get(apiUrl,{
+        headers: {
+          'Authorization': `Bearer ${accessToken}`
+        },
+        params:{
+          requestData,
+        },
+      })
+      .then(res => {
+        console.log(res);
+        if (res.data.status_code === 'SYSTEM_1000') {
+          this.lists = res.data.data
+        }
+      });
+    },
+  },
 }
 </script>
 
@@ -44,7 +77,9 @@ export default {
   width: 465px;
   height: 262px;
   border-radius: 20px;
-  background-image: url('../../../assets/images/products/concert4.jpg');
+  & img{
+    width: 100%;
+  }
   @media(max-width: 576px) {
     width: 305px;
     height: 172px;
