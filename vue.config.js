4617f5e4ada59c5793b6ae6ce972a18d6746c113
michaellee17 <missing821017@yahoo.com.tw>
2023-08-15 17:31:54 +0800

    add:嘗試首頁ssg

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+//原版
 const { defineConfig } = require('@vue/cli-service');
 const path = require("path");
 
@@ -16,12 +17,36 @@ module.exports = defineConfig(
                 })
             ]
         }
-        // configureWebpack: {
-        //     resolve: {
-        //       alias: {
-        //         "@": path.resolve(__dirname, "./src")
-        //       }
-        //     }
-        //   }
     }
 )
+//開發中
+// const { defineConfig } = require('@vue/cli-service')
+// const path = require('path');
+// const PrerenderSPAPlugin = require('prerender-spa-plugin');
+// const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
+
+// module.exports = defineConfig({
+//   configureWebpack: config => {
+//     if (process.env.NODE_ENV === 'production') {
+//       return {
+//         plugins: [
+//           new PrerenderSPAPlugin({
+//             staticDir: path.join(__dirname, 'dist'),
+//             routes: ['/'],
+//             renderer: new Renderer({
+//               inject: {
+//                 foo: 'bar'
+//               },
+//               headless: true, 
+//               renderAfterDocumentEvent: 'render-event',
+//               navigationParams: {
+//                 timeout: 0
+//               }
+//             })
+//           })
+//         ],
+//       }
+//     }
+//   },
+// })
+
